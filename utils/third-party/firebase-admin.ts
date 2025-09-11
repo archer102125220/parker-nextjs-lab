50f76bcb47a9a5c876497fbf20d01129705c1d73
Parker <archer102125220.2015@gmail.com>
2025-09-11 17:56:44 +0800

    update: yarn add firebase, firebase-admin

---
+++ utils/third-party/firebase-admin.ts
@@ -0,0 +1,133 @@
+import firebaseAdmin from 'firebase-admin';
+
+// https://firebase.google.com/docs/cloud-messaging/migrate-v1?authuser=0&_gl=11yc83x4_gaMTMzNDU0MDY5LjE2ODYxMDI5NzQ._ga_CW55HF8NVT*MTY5NDY3NzAxMy4yNC4xLjE2OTQ2NzgxODEuMC4wLjA.&hl=zh-tw#linux-or-macos
+// https://firebase.google.com/docs/cloud-messaging/js/client?hl=zh-tw#web
+// https://firebase.google.com/docs/cloud-messaging/js/client?hl=zh-tw#web_2
+
+export const CREDENTIAL = process.env.NEXT_PUBLIC_FIREBASE_CREDENTIAL || '{}';
+
+export const FIREBASE_ADMIN_CONFIG = {
+  authDomain: 'parker-nuxt-lab.firebaseapp.com',
+  projectId: 'parker-nuxt-lab',
+  storageBucket: 'parker-nuxt-lab.firebasestorage.app'
+};
+
+export let firebaseAdminWeb: firebaseAdmin.app.App | null = null;
+export function getFirebaseAdminWeb() {
+  return firebaseAdminWeb;
+}
+
+export const ANDROID_CREDENTIAL =
+  process.env.NEXT_PUBLIC_ANDROID_FIREBASE_CREDENTIAL || '{}';
+
+export const ANDROID_FIREBASE_ADMIN_CONFIG = {
+  authDomain: 'httpsbibiancojp.firebaseapp.com',
+  projectId: 'httpsbibiancojp',
+  storageBucket: 'httpsbibiancojp.appspot.com'
+};
+export let firebaseAdminAndroid: firebaseAdmin.app.App | null = null;
+export function getFirebaseAdminAndroid() {
+  return firebaseAdminAndroid;
+}
+
+export const IOS_CREDENTIAL =
+  process.env.NEXT_PUBLIC_IOS_FIREBASE_CREDENTIAL || '{}';
+
+export const IOS_FIREBASE_ADMIN_CONFIG = {
+  authDomain: 'httpsbibiancojp.firebaseapp.com',
+  projectId: 'httpsbibiancojp',
+  storageBucket: 'httpsbibiancojp.appspot.com'
+};
+export let firebaseAdminIos: firebaseAdmin.app.App | null = null;
+export function getFirebaseAdminIos() {
+  return firebaseAdminIos;
+}
+
+export function getFirebaseAdmin(
+  name = '[DEFAULT]'
+): firebaseAdmin.app.App | null {
+  let firebaseAdminApp = null;
+  try {
+    firebaseAdminApp = firebaseAdmin.app(name);
+  } catch (error) {
+    console.error(error);
+  }
+  return firebaseAdminApp;
+}
+
+export function firebaseAdminServerInit() {
+  try {
+    if (typeof window !== 'undefined') return;
+
+    if (getFirebaseAdmin('[DEFAULT]')) {
+      firebaseAdminWeb = getFirebaseAdmin('[DEFAULT]');
+    } else if (
+      typeof CREDENTIAL === 'string' &&
+      CREDENTIAL !== '{}' &&
+      CREDENTIAL !== ''
+    ) {
+      firebaseAdminWeb = firebaseAdmin.initializeApp({
+        ...FIREBASE_ADMIN_CONFIG,
+        credential: firebaseAdmin.credential.cert(JSON.parse(CREDENTIAL))
+      });
+    }
+
+    if (getFirebaseAdmin('androidFirebaseAdmin')) {
+      firebaseAdminAndroid = getFirebaseAdmin('androidFirebaseAdmin');
+    } else if (
+      typeof ANDROID_CREDENTIAL === 'string' &&
+      ANDROID_CREDENTIAL !== '{}' &&
+      ANDROID_CREDENTIAL !== ''
+    ) {
+      firebaseAdminAndroid = firebaseAdmin.initializeApp(
+        {
+          ...ANDROID_FIREBASE_ADMIN_CONFIG,
+          credential: firebaseAdmin.credential.cert(
+            JSON.parse(ANDROID_CREDENTIAL)
+          )
+        },
+        'androidFirebaseAdmin'
+      );
+    }
+
+    if (getFirebaseAdmin('iosFirebaseAdmin')) {
+      firebaseAdminIos = getFirebaseAdmin('iosFirebaseAdmin');
+    } else if (
+      typeof IOS_CREDENTIAL === 'string' &&
+      IOS_CREDENTIAL !== '{}' &&
+      IOS_CREDENTIAL !== ''
+    ) {
+      firebaseAdminIos = firebaseAdmin.initializeApp(
+        {
+          ...IOS_FIREBASE_ADMIN_CONFIG,
+          credential: firebaseAdmin.credential.cert(JSON.parse(IOS_CREDENTIAL))
+        },
+        'iosFirebaseAdmin'
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (typeof firebaseAdminWeb !== 'object' || firebaseAdminWeb === null) {
+    console.warn('Firebase Admin app initialization failed.');
+  }
+
+  if (
+    typeof firebaseAdminAndroid !== 'object' ||
+    firebaseAdminAndroid === null
+  ) {
+    console.warn('Android Firebase Admin app initialization failed.');
+  }
+
+  if (typeof firebaseAdminIos !== 'object' || firebaseAdminIos === null) {
+    console.warn('IOS Firebase Admin app initialization failed.');
+  }
+
+  return { firebaseAdminWeb, firebaseAdminAndroid, firebaseAdminIos };
+}
+
+// https://ithelp.ithome.com.tw/articles/10269342
+// https://vercel.com/archer102125220/resume-web
+
+firebaseAdminServerInit();
---
+++ utils/third-party/firebase.ts
@@ -0,0 +1,469 @@
+import type { FirebaseOptions, FirebaseApp } from 'firebase/app';
+import { initializeApp, initializeServerApp } from 'firebase/app';
+import type { Analytics } from 'firebase/analytics';
+import {
+  getAnalytics,
+  logEvent,
+  isSupported as analyticsIsSupported
+} from 'firebase/analytics';
+import type { Firestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore/lite';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import type { Messaging } from 'firebase/messaging';
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported as messagingIsSupported
+} from 'firebase/messaging';
+
+// import { POST_registerMessageToken } from '@/services/client/firebase-admin';
+
+// https://firebase.google.com/docs/cloud-messaging/js/receive?hl=zh-cn#web-version-9_2
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v11.10.0 and later, measurementId is optional
+export const FIREBASE_CONFIG: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'parker-nuxt-lab.firebaseapp.com',
+  projectId: 'parker-nuxt-lab',
+  storageBucket: 'parker-nuxt-lab.firebasestorage.app',
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_GA_ID
+};
+
+export class firebase {
+  constructor(config = FIREBASE_CONFIG) {
+    this.#_firebaseConfig =
+      (typeof config === 'object' ? config : FIREBASE_CONFIG) ||
+      FIREBASE_CONFIG;
+
+    this.logEvent = this.logEvent.bind(this);
+    this.analyticsLog = this.analyticsLog.bind(this);
+    this.configUpdate = this.configUpdate.bind(this);
+    this.croeServerInit = this.croeServerInit.bind(this);
+    this.croeClientInit = this.croeClientInit.bind(this);
+    this.croeInit = this.croeInit.bind(this);
+    this.appInit = this.appInit.bind(this);
+    this.appServerInit = this.appServerInit.bind(this);
+    this.appClientInit = this.appClientInit.bind(this);
+    this.analyticsInit = this.analyticsInit.bind(this);
+    this.getNotificationPermission = this.getNotificationPermission.bind(this);
+    this.requestNotificationPermission =
+      this.requestNotificationPermission.bind(this);
+    this.getServiceWorker = this.getServiceWorker.bind(this);
+    this.registerServiceWorker = this.registerServiceWorker.bind(this);
+    this.defaultSaveToken = this.defaultSaveToken.bind(this);
+    this.messagingInit = this.messagingInit.bind(this);
+  }
+  #_vapidKey = process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY;
+  #_firebaseConfig: FirebaseOptions | null = null;
+  #_serviceWorkerScope = '/';
+  #_serviceWorkerPath = '/service-worker.js';
+  #_serviceWorker: ServiceWorkerRegistration | null = null;
+  #_croe: FirebaseApp | null = null;
+  #_croeInited = false;
+  #_store: Firestore | null = null;
+  #_storeInited = false;
+  #_token: string | null = null;
+  #_messaging: Messaging | null = null;
+  #_messagingInited = false;
+  #_analytics: Analytics | null = null;
+  #_analyticsInited = false;
+
+  get firebaseConfig(): FirebaseOptions | null {
+    return this.#_firebaseConfig;
+  }
+  get serviceWorkerScope() {
+    return this.#_serviceWorkerScope;
+  }
+  get serviceWorkerPath() {
+    return this.#_serviceWorkerPath;
+  }
+  get serviceWorker() {
+    return this.#_serviceWorker;
+  }
+  get croe(): FirebaseApp | null {
+    return this.#_croe;
+  }
+  get croeInited() {
+    return this.#_croeInited;
+  }
+  get store(): Firestore | null {
+    return this.#_store;
+  }
+  get storeInited() {
+    return this.#_storeInited;
+  }
+  get token() {
+    return this.#_token;
+  }
+  get messaging(): Messaging | null {
+    return this.#_messaging;
+  }
+  get messagingInited() {
+    return this.#_messagingInited;
+  }
+  get analytics() {
+    return {
+      app: this.#_analytics,
+      log: this.logEvent
+    };
+  }
+  get analyticsInited() {
+    return this.#_analyticsInited;
+  }
+
+  initializeApp = initializeApp;
+  initializeServerApp = initializeServerApp;
+  analyticsIsSupported = analyticsIsSupported;
+  messagingIsSupported = messagingIsSupported;
+  getFirestore = getFirestore;
+  getAnalytics = getAnalytics;
+  getToken = getToken;
+  getMessaging = getMessaging;
+  onMessage = onMessage;
+
+  logEvent(
+    analyticsApp = this.analytics?.app,
+    eventName: string,
+    eventParams?: {
+      [key: string]: any;
+    }
+  ) {
+    if (typeof analyticsApp !== 'object' || analyticsApp === null) {
+      console.warn('firebase analytics missing');
+      return;
+    }
+    return logEvent(analyticsApp, eventName, eventParams);
+  }
+  analyticsLog(
+    eventName: string,
+    eventParams?: {
+      [key: string]: any;
+    }
+  ) {
+    if (
+      this.analyticsInited !== true &&
+      (typeof this.#_analytics !== 'object' || this.#_analytics === null)
+    ) {
+      console.warn('firebase analytics missing');
+      return;
+    }
+    return this.logEvent(this.analytics?.app, eventName, eventParams);
+  }
+  async configUpdate(newConfig = null, reInit = true) {
+    if (typeof newConfig !== 'object' || newConfig === null) {
+      throw new Error('invalid config');
+    }
+    this.#_firebaseConfig = newConfig;
+
+    if (reInit === true) {
+      this.croeInit();
+      await this.appInit();
+    }
+  }
+
+  // Initialize server Firebase
+  croeServerInit(firebaseConfig = this.firebaseConfig) {
+    if (typeof window === 'object') return { firebaseCroe: this.croe };
+    this.#_croeInited = false;
+
+    try {
+      if (firebaseConfig === null) throw new Error('firebaseConfig error');
+      const newFirebaseCroe = this.initializeServerApp(firebaseConfig);
+      this.#_croe = newFirebaseCroe;
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.#_croeInited = true;
+    return { firebaseCroe: this.croe };
+  }
+
+  // Initialize client Firebase
+  croeClientInit(firebaseConfig = this.firebaseConfig) {
+    if (typeof window === 'undefined') return { firebaseCroe: this.croe };
+    this.#_croeInited = false;
+
+    try {
+      if (firebaseConfig === null) throw new Error('firebaseConfig error');
+      const newFirebaseCroe = this.initializeApp(firebaseConfig);
+      this.#_croe = newFirebaseCroe;
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.#_croeInited = true;
+    return { firebaseCroe: this.croe };
+  }
+
+  // Initialize Firebase
+  croeInit(firebaseConfig = this.firebaseConfig) {
+    if (typeof window === 'undefined') {
+      return this.croeServerInit(firebaseConfig);
+    } else {
+      return this.croeClientInit(firebaseConfig);
+    }
+  }
+
+  // Initialize Firebase App
+  async appInit(currentFirebaseCroe = this.croe) {
+    if (typeof window === 'undefined') {
+      return await this.appServerInit(currentFirebaseCroe);
+    } else {
+      return await this.appClientInit(currentFirebaseCroe);
+    }
+  }
+
+  appServerInit(currentFirebaseCroe = this.croe) {
+    this.firestoreInit(currentFirebaseCroe);
+
+    return { firebaseCroe: currentFirebaseCroe, store: this.store };
+  }
+
+  async appClientInit(currentFirebaseCroe = this.croe) {
+    await Promise.all([
+      this.analyticsInit(currentFirebaseCroe),
+      this.messagingInit(currentFirebaseCroe)
+    ]);
+
+    this.firestoreInit(currentFirebaseCroe);
+
+    return {
+      firebaseCroe: currentFirebaseCroe,
+      firebaseAnalytics: this.analytics,
+      firestore: this.#_store
+    };
+  }
+
+  firestoreInit(currentFirebaseCroe = this.croe) {
+    this.#_storeInited = false;
+
+    try {
+      if (currentFirebaseCroe === null) throw new Error('firebaseCroe missing');
+      const newFirebaseDB = this.getFirestore(currentFirebaseCroe);
+      this.#_store = newFirebaseDB;
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.#_storeInited = true;
+    return this.#_store;
+  }
+
+  async analyticsInit(currentFirebaseCroe = this.croe) {
+    if (typeof window === 'undefined') return this.analytics;
+
+    this.#_analyticsInited = false;
+
+    const isAnalyticsSupport = await this.analyticsIsSupported();
+
+    if (isAnalyticsSupport === false)
+      console.warn('firebase analytics is not Supported');
+
+    try {
+      if (isAnalyticsSupport === true) {
+        if (currentFirebaseCroe === null) {
+          throw new Error('firebaseCroe missing');
+        }
+        const newFirebaseAnalytics = this.getAnalytics(currentFirebaseCroe);
+        this.#_analytics = newFirebaseAnalytics;
+      }
+
+      this.#_analyticsInited = true;
+    } catch (error) {
+      console.error(error);
+    }
+
+    return this.analytics;
+  }
+  getNotificationPermission() {
+    if (
+      typeof window === 'undefined' ||
+      typeof window?.Notification === 'undefined'
+    ) {
+      return false;
+    }
+    return Notification.permission === 'granted';
+  }
+  async requestNotificationPermission() {
+    try {
+      const isMessagingSupport = await this.messagingIsSupported();
+
+      if (isMessagingSupport === false) {
+        console.warn('FCM is not Supported');
+        return false;
+      }
+      console.log('Requesting permission...');
+
+      console.log({ ['Notification.permission']: Notification.permission });
+      if (Notification.permission === 'granted') {
+        console.log('Notification permission granted.');
+        return true;
+      } else if (Notification.permission !== 'denied') {
+        const permission = await Notification.requestPermission();
+        console.log({ permission });
+        if (permission === 'granted') {
+          console.log('Notification permission granted.');
+          return true;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    return false;
+  }
+  async getServiceWorker(scope = this.serviceWorkerScope) {
+    if (
+      'serviceWorker' in navigator &&
+      typeof window.navigator.serviceWorker !== 'undefined'
+    ) {
+      const serviceWorker =
+        await window.navigator.serviceWorker.getRegistration(scope);
+      if (serviceWorker) {
+        this.#_serviceWorkerScope = scope;
+        this.#_serviceWorker = serviceWorker;
+
+        return serviceWorker;
+      }
+    }
+    // throw new Error('The browser doesn`t support service worker.');
+
+    return null;
+  }
+  async registerServiceWorker(
+    scope = this.serviceWorkerScope,
+    serviceWorkerPath = this.serviceWorkerPath
+  ) {
+    if (
+      'serviceWorker' in navigator &&
+      typeof window.navigator.serviceWorker !== 'undefined'
+    ) {
+      const serviceWorker = await window.navigator.serviceWorker.register(
+        serviceWorkerPath,
+        {
+          scope
+        }
+      );
+
+      this.#_serviceWorkerScope = scope;
+      this.#_serviceWorker = serviceWorker;
+
+      return serviceWorker;
+    }
+    // throw new Error('The browser doesn`t support service worker.');
+
+    return null;
+  }
+  async getOrRegisterServiceWorker(scope = this.serviceWorkerScope) {
+    const serviceWorker = await this.getServiceWorker(scope);
+    if (serviceWorker) return serviceWorker;
+
+    return await this.registerServiceWorker(scope);
+  }
+  async defaultSaveToken(token: string | null) {
+    console.log({ token });
+    return;
+  }
+  async messagingInit(
+    currentFirebaseCroe = this.croe,
+    saveToken = this.defaultSaveToken
+  ) {
+    if (typeof window === 'undefined') return this.messaging;
+
+    const isMessagingSupport = await this.messagingIsSupported();
+    if (isMessagingSupport === false) console.warn('FCM is not Supported');
+
+    const permission = this.getNotificationPermission();
+    if (permission !== true)
+      console.warn('firebaseMessagingInit: Notification Permissio.');
+
+    if (isMessagingSupport === true && permission === true) {
+      this.#_messagingInited = false;
+
+      try {
+        const serviceWorkerRegistration = await this.getServiceWorker();
+
+        if (
+          typeof serviceWorkerRegistration === 'undefined' ||
+          serviceWorkerRegistration === null
+        ) {
+          throw new Error("The browser doesn't support service worker.");
+        }
+
+        if (currentFirebaseCroe === null) {
+          throw new Error('firebaseCroe missing');
+        }
+        const newFirebaseMessaging = this.getMessaging(currentFirebaseCroe);
+        this.#_messaging = newFirebaseMessaging;
+
+        const token = await this.getToken(newFirebaseMessaging, {
+          vapidKey: this.#_vapidKey,
+          serviceWorkerRegistration
+        });
+        this.#_token = token;
+
+        await saveToken(token);
+
+        /*
+          interface MessagePayload {
+            readonly collapseKey: string; // 僅限 FCM 訊息才有
+            readonly from: string;       // 訊息的發送者
+            readonly messageId: string;  // 訊息的唯一 ID
+            readonly messageType: string; // "push" 或 "data"
+  
+            readonly data?: { [key: string]: string }; // 如果有資料訊息，則包含此屬性
+  
+            readonly notification?: NotificationPayload; // 如果有通知訊息，則包含此屬性
+  
+            readonly rawData: object; // 原始訊息數據，可能包含更多低級別屬性
+          }
+  
+          interface NotificationPayload {
+            readonly title: string | undefined;
+            readonly body: string | undefined;
+            readonly image: string | undefined; // 如果設定了通知圖片
+            // 還有其他可能的標準通知屬性，例如 icon, badge, click_action, tag, etc.
+            // 這些屬性通常在 `notification` 物件內部，或者作為 `data` 的一部分，視如何發送而定
+          }
+        */
+
+        this.onMessage(newFirebaseMessaging, (payload) => {
+          try {
+            // new Notification('測試', {
+            //   body: payload.data?.msg,
+            //   icon: '/img/favicon/favicon.ico'
+            // });
+
+            const notificationTitle =
+              payload?.notification?.title || payload?.data?.title || '';
+            const notificationBody =
+              payload?.notification?.body || payload?.data?.msg || '';
+            const notificationIcon =
+              payload?.notification?.image || payload?.data?.img || '';
+
+            serviceWorkerRegistration.showNotification(notificationTitle, {
+              body: notificationBody,
+              icon: notificationIcon || '/img/favicon/favicon.ico'
+            });
+          } catch (error) {
+            console.error(error);
+          }
+        });
+
+        this.#_messagingInited = true;
+
+        return newFirebaseMessaging;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    return this.messaging;
+  }
+}
+// export const Firebase = new firebase();
