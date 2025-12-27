117ba9a994cb41936f3ff46d2a3dc53b985a7e24
Parker <archer102125220.2015@gmail.com>
2025-12-27 20:25:11 +0800

    feat: introduce `loading.tsx` files for numerous Next.js app routes.

---
+++ app/[locale]/route/query-back-test/loading.tsx
@@ -0,0 +1,11 @@
+import { headers } from 'next/headers';
+
+import { PageLoading } from '@/components/PageLoading';
+
+export default async function Loading() {
+  const nonce = (await headers()).get('x-nonce') || '';
+
+  // Or a custom loading skeleton component
+  // return <p>Loading...</p>;
+  return <PageLoading loading={true} nonce={nonce} />;
+}
